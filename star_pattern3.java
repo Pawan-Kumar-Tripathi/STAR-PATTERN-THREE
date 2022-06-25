584b2414b60c5f1cc9ca2637e815d655d2e50550
PAWAN KUMAR TRIPATHI <78968923+Pawan-Kumar-Tripathi@users.noreply.github.com>
2022-06-26 00:02:19 +0530

    Add files via upload

---
+++ star_pattern3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class star_pattern3
+    {
+        public void main()
+        
+        {
+            for(int i =0;i<4;i++)
+            {
+            for(int j=0;j<4;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+    
+            
